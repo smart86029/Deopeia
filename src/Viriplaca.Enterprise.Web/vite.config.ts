029473b925ca9cff8aa88ff09454bbca570d5a7d
Bill Hsu <smart86029@gmail.com>
2024-03-04 17:18:03 +0800

    chore: define service port

--- src/Viriplaca.Enterprise.Web/vite.config.ts
+++ src/Viriplaca.Enterprise.Web/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:58309/',
+        target: 'http://localhost:9000/',
         changeOrigin: true,
       },
     },
