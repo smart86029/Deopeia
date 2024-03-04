9e60bcaef4925abac57a7bbcde0e2acbd138d4cb
Bill Hsu <smart86029@gmail.com>
2024-03-04 17:35:15 +0800

    fix(hr): T-SQL offset and fetch syntax

--- src/Viriplaca.HR.App/Departments/GetDepartments/DepartmentDto.cs
+++ src/Viriplaca.HR.App/Departments/GetDepartments/DepartmentDto.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Viriplaca.HR.App.Departments.GetDepartments;
 
 public class DepartmentDto
@@ -10,7 +12,13 @@ public class DepartmentDto
 
     public Guid? ParentId { get; set; }
 
-    public string HeadName { get; set; } = string.Empty;
+    [JsonIgnore]
+    public string FirstName { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    public string? LastName { get; set; } = string.Empty;
+
+    public string HeadName => $"{FirstName} {LastName}";
 
     public int EmployeeCount { get; set; }
 }
--- src/Viriplaca.HR.Data/Departments/GetDepartments/GetDepartmentsQueryHandler.cs
+++ src/Viriplaca.HR.Data/Departments/GetDepartments/GetDepartmentsQueryHandler.cs
@@ -24,13 +24,14 @@ internal class GetDepartmentsQueryHandler(SqlConnection connection)
             return result;
         }
 
-        var sql = $@"
+        var sql = builder.AddTemplate(@"
 SELECT
     A.Id,
     A.Name,
     A.IsEnabled,
     A.ParentId,
-    D.Name AS HeadName,
+    D.FirstName,
+    D.LastName,
     B.EmployeeCount
 FROM HR.Department AS A
 LEFT JOIN (
@@ -45,16 +46,16 @@ LEFT JOIN (
     WHERE (EndedAt IS NULL OR EndedAt >= GETDATE()) AND IsHead = 1
 ) AS C ON A.Id = C.DepartmentId
 LEFT JOIN (
-    SELECT Id, Name
+    SELECT Id, FirstName, LastName
     FROM HR.Person
 ) AS D ON C.EmployeeId = D.Id
 /**where**/
 ORDER BY Id
-LIMIT @Limit
-OFFSET @Offset
-";
+OFFSET @Offset ROWS 
+FETCH NEXT @Limit ROWS ONLY
+");
         builder.AddParameters(new { result.Limit, result.Offset });
-        var departments = await _connection.QueryAsync<DepartmentDto>(sql, request);
+        var departments = await _connection.QueryAsync<DepartmentDto>(sql.RawSql, sql.Parameters);
         result.Items = departments.ToList();
 
         return result;
