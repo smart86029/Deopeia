b88f8e5acf2e105a49a3b85f47c49450ee98057a
Bill Hsu <smart86029@gmail.com>
2024-03-22 17:49:42 +0800

    chore: add MinIO and Nginx

--- src/Viriplaca.Enterprise.Web/vite.config.ts
+++ src/Viriplaca.Enterprise.Web/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9000/',
+        target: 'http://localhost:7000/',
         changeOrigin: true,
       },
     },
