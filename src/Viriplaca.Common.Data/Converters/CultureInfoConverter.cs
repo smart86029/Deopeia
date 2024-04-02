f925b79cf939d6ec098906a79769d2474093a780
Bill Hsu <smart86029@gmail.com>
2024-04-02 16:19:41 +0800

    refactor(common): use primary constructor

--- src/Viriplaca.Common.Data/Converters/CultureInfoConverter.cs
+++ src/Viriplaca.Common.Data/Converters/CultureInfoConverter.cs
@@ -2,13 +2,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Viriplaca.Common.Data.Converters;
 
-internal class CultureInfoConverter : ValueConverter<CultureInfo, string>
+internal class CultureInfoConverter()
+    : ValueConverter<CultureInfo, string>(
+        culture => culture.Name,
+        name => CultureInfo.GetCultureInfo(name))
 {
-    private static readonly Expression<Func<CultureInfo, string>> _convertTo = x => x.Name;
-    private static readonly Expression<Func<string, CultureInfo>> _convertFrom = x => CultureInfo.GetCultureInfo(x);
-
-    public CultureInfoConverter()
-        : base(_convertTo, _convertFrom)
-    {
-    }
 }
--- src/Viriplaca.Common.Data/Converters/DateTimeOffsetConverter.cs
+++ src/Viriplaca.Common.Data/Converters/DateTimeOffsetConverter.cs
@@ -2,13 +2,9 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Viriplaca.Common.Data.Converters;
 
-internal class DateTimeOffsetConverter : ValueConverter<DateTimeOffset, DateTime>
+internal class DateTimeOffsetConverter()
+    : ValueConverter<DateTimeOffset, DateTime>(
+        dateTimeOffset => dateTimeOffset.UtcDateTime,
+        dateTime => new DateTimeOffset(dateTime))
 {
-    private static readonly Expression<Func<DateTimeOffset, DateTime>> _convertTo = x => x.UtcDateTime;
-    private static readonly Expression<Func<DateTime, DateTimeOffset>> _convertFrom = x => new DateTimeOffset(x);
-
-    public DateTimeOffsetConverter()
-        : base(_convertTo, _convertFrom)
-    {
-    }
 }
