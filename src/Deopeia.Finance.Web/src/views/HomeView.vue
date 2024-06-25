04a8316f030e7f443951fc8546a2288165e0a594
Bill Hsu <smart86029@gmail.com>
2024-06-25 18:12:20 +0800

    feat(finance): add package

--- src/Deopeia.Finance.Web/src/views/HomeView.vue
+++ src/Deopeia.Finance.Web/src/views/HomeView.vue
@@ -1,9 +1,52 @@
+<template>
+  <el-container>
+    <el-header>
+      <MainHeader />
+    </el-header>
+    <el-container>
+      <el-aside class="menu-aside" :class="{ close: asideSwitch }">
+        <el-scrollbar>
+          <AsideMenu />
+        </el-scrollbar>
+      </el-aside>
+      <el-main>
+        <el-scrollbar view-class="scrollbar-view">
+          <RouterView />
+        </el-scrollbar>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import commonApi from '@/api/common-api';
+// import { useAuthStore } from '@/stores/auth';
+// import { useGlobal } from '@/stores/global';
+
+const asideSwitch = ref(false);
+
+// commonApi.getGlobal().then((res) => {
+//   imageDomain.value = res.data.imageDomain;
+//   navigationUrl.value = res.data.navigationUrl;
+//   userLevel.value = res.data.userLevel;
+// });
+// const authStore = useAuthStore();
+// // 監聽切換分頁回來, 會主動打refresh
+// document.addEventListener('visibilitychange', function () {
+//   if (document.visibilityState === 'visible') {
+//     authStore.refresh();
+//   }
+// });
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped lang="scss">
+.menu-aside {
+  width: var(--aside-menu-width);
+  transition: all 0.3s cubic-bezier(0, 0, 0.5, 1);
+  &.close {
+    width: calc(
+      var(--el-menu-icon-width) + var(--el-menu-base-level-padding) * 2
+    );
+  }
+}
+</style>
--- src/Deopeia.Finance.Web/vite.config.ts
+++ src/Deopeia.Finance.Web/vite.config.ts
@@ -1,16 +1,58 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import externalGlobals from 'rollup-plugin-external-globals';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue', 'vue-i18n', 'vue-router', 'pinia', '@vueuse/core'],
+      dts: './src/auto-imports.d.ts',
+    }),
+    Components({
+      dts: './src/components.d.ts',
+    }),
+    {
+      ...externalGlobals({
+        vue: 'Vue',
+        'vue-demi': 'VueDemi',
+        'vue-i18n': 'VueI18n',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        'element-plus': 'ElementPlus',
+      }),
+      enforce: 'post',
+      apply: 'build',
+    },
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7099/',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'vue',
+        'vue-demi',
+        'vue-i18n',
+        'vue-router',
+        'axios',
+        'element-plus',
+      ],
+    },
+  },
+});
