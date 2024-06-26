2bfc163f166bce3e3044ec18a7e2d3c24be55bac
Bill Hsu <smart86029@gmail.com>
2024-06-26 14:51:59 +0800

    feat(quote): add quote projects

--- src/Deopeia.AppHost/Program.cs
+++ src/Deopeia.AppHost/Program.cs
@@ -6,6 +6,7 @@ var minIOSecretKey = builder.AddParameter("MinIOSecretKey");
 
 var sql = builder.AddSqlServer("sql");
 var dbIdentity = sql.AddDatabase("Identity");
+var dbQuote = sql.AddDatabase("Quote");
 
 builder
     .AddProject<Projects.Deopeia_Identity_Api>("deopeia-identity-api")
@@ -13,5 +14,9 @@ builder
     .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
     .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
     .WithReference(dbIdentity);
+
+builder.AddProject<Projects.Deopeia_Quote_Api>("deopeia-quote-api").WithReference(dbQuote);
+
+builder.AddProject<Projects.Deopeia_Quote_Worker>("deopeia-quote-worker");
 
 builder.Build().Run();
