73c42db834c3935269c14721ee8f5c1599284029
Bill Hsu <smart86029@gmail.com>
2024-10-16 17:36:53 +0800

    fix(quote): use fallback culture

--- src/Deopeia.Quote.Infrastructure/Instruments/GetInstrument/GetInstrumentQueryHandler.cs
+++ src/Deopeia.Quote.Infrastructure/Instruments/GetInstrument/GetInstrumentQueryHandler.cs
@@ -15,14 +15,20 @@ internal class GetInstrumentQueryHandler(NpgsqlConnection connection)
         var sql = """
 SELECT
     a.symbol,
-    b.name
+    COALESCE(b.name, c.name) AS name
 FROM instrument AS a
-INNER JOIN instrument_locale AS b ON a.id = b.instrument_id AND b.culture = @Culture
+LEFT JOIN instrument_locale AS b ON a.id = b.instrument_id AND b.culture = @CurrentCulture
+INNER JOIN instrument_locale AS c ON a.id = c.instrument_id AND c.culture = @DefaultThreadCurrentCulture
 WHERE a.symbol = @Symbol
 """;
         var result = await _connection.QuerySingleAsync<GetInstrumentViewModel>(
             sql,
-            new { request.Symbol, Culture = CultureInfo.CurrentCulture }
+            new
+            {
+                CultureInfo.CurrentCulture,
+                CultureInfo.DefaultThreadCurrentCulture,
+                request.Symbol,
+            }
         );
 
         return result;
