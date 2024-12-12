bac167c1267c4c46a16d4931c3f3b52f0f7b9851
Bill Hsu <smart86029@gmail.com>
2024-12-12 18:13:43 +0800

    feat(finance): add buy and sell link

--- src/Deopeia.Finance.Web/src/views/market/MarketForex.vue
+++ src/Deopeia.Finance.Web/src/views/market/MarketForex.vue
@@ -14,10 +14,20 @@
     <el-table-column prop="volume" :label="$t('finance.volume')" />
     <el-table-column :label="$t('route.trading')">
       <template #default="{ row }">
-        <TextLink
-          :to="{ name: 'symbol.view', params: { symbol: row.symbol } }"
-          :text="row.symbol"
-        />
+        <el-button
+          size="small"
+          :type="positive"
+          @click="$router.push({ name: 'trading' })"
+        >
+          {{ $t(`trading.orderSide.${OrderSide.Buy}`) }}
+        </el-button>
+        <el-button
+          size="small"
+          :type="negative"
+          @click="$router.push({ name: 'trading' })"
+        >
+          {{ $t(`trading.orderSide.${OrderSide.Sell}`) }}
+        </el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -34,13 +44,16 @@ import marketApi, {
   type Forex,
   type GetForexQuery,
 } from '@/api/market/market-api';
-
 import {
   defaultQuery,
   defaultResult,
   reassign,
   type PageResult,
 } from '@/models/page';
+import { OrderSide } from '@/models/trading/order-side';
+import { usePreferencesStore } from '@/stores/preferences';
+
+const { positive, negative } = storeToRefs(usePreferencesStore());
 
 const loading = ref(false);
 const query: GetForexQuery = reactive({
