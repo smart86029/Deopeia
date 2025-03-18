4cb4fa5870eca406edc5002dcdcb43f269368460
Bill Hsu <smart86029@gmail.com>
2025-03-18 13:50:48 +0800

    refactor: encapsulate shared reference

--- src/Deopeia.AppHost/Program.cs
+++ src/Deopeia.AppHost/Program.cs
@@ -2,14 +2,6 @@ using Deopeia.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var jwtKey = builder.AddParameter("JwtKey");
-var jwtIssuer = builder.AddParameter("JwtIssuer");
-var minIOEndpoint = builder.AddParameter("MinIOEndpoint");
-var minIOAccessKey = builder.AddParameter("MinIOAccessKey");
-var minIOSecretKey = builder.AddParameter("MinIOSecretKey");
-
-var kafka = builder.AddKafka("kafka").WithKafkaUI(x => x.WithHostPort(9100)).WithDataVolume();
-
 var password = builder.AddParameter("postgresql-password", secret: true);
 var postgres = builder.AddPostgres("postgres", password: password, port: 59999).WithDataVolume();
 var dbIdentity = postgres.AddDatabase("identity");
@@ -18,57 +10,45 @@ var dbTrading = postgres.AddDatabase("trading");
 
 var identityApi = builder
     .AddProject<Projects.Deopeia_Identity_Api>("deopeia-identity-api")
-    .WithEnvironment("Jwt__Key", jwtKey)
-    .WithEnvironment("Jwt__Issuer", jwtIssuer)
-    .WithEnvironment("MinIO__Endpoint", minIOEndpoint)
-    .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
-    .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
-    .WithReferenceAndWaitFor(kafka)
-    .WithReferenceAndWaitFor(dbIdentity);
-identityApi.WithEnvironment("Proxy", identityApi.GetEndpoint("https"));
+    .WithJwt()
+    .WithMinIO()
+    .WithKafka()
+    .WithReferenceAndWaitFor(dbIdentity)
+    .WithProxyEndpoint();
 
 var notificationHub = builder
     .AddProject<Projects.Deopeia_Notification_Hub>("deopeia-notification-hub")
-    .WithReferenceAndWaitFor(kafka);
+    .WithKafka();
 
 var quoteApi = builder
     .AddProject<Projects.Deopeia_Quote_Api>("deopeia-quote-api")
-    .WithEnvironment("MinIO__Endpoint", minIOEndpoint)
-    .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
-    .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
-    .WithReferenceAndWaitFor(kafka)
-    .WithReferenceAndWaitFor(dbQuote);
-quoteApi.WithEnvironment("Proxy", quoteApi.GetEndpoint("http"));
+    .WithMinIO()
+    .WithKafka()
+    .WithReferenceAndWaitFor(dbQuote)
+    .WithProxyEndpoint();
 
 builder
     .AddProject<Projects.Deopeia_Quote_Worker>("deopeia-quote-worker")
-    .WithEnvironment("MinIO__Endpoint", minIOEndpoint)
-    .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
-    .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
-    .WithReferenceAndWaitFor(kafka)
+    .WithMinIO()
+    .WithKafka()
     .WithReferenceAndWaitFor(dbQuote);
 
 var tradingApi = builder
     .AddProject<Projects.Deopeia_Trading_Api>("deopeia-trading-api")
-    .WithEnvironment("MinIO__Endpoint", minIOEndpoint)
-    .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
-    .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
-    .WithReferenceAndWaitFor(kafka)
-    .WithReferenceAndWaitFor(dbTrading);
-tradingApi.WithEnvironment("Proxy", tradingApi.GetEndpoint("http"));
+    .WithMinIO()
+    .WithKafka()
+    .WithReferenceAndWaitFor(dbTrading)
+    .WithProxyEndpoint();
 
 builder
     .AddProject<Projects.Deopeia_Trading_Worker>("deopeia-trading-worker")
-    .WithEnvironment("MinIO__Endpoint", minIOEndpoint)
-    .WithEnvironment("MinIO__AccessKey", minIOAccessKey)
-    .WithEnvironment("MinIO__SecretKey", minIOSecretKey)
-    .WithReferenceAndWaitFor(kafka)
+    .WithMinIO()
+    .WithKafka()
     .WithReferenceAndWaitFor(dbTrading);
 
 builder
     .AddProject<Projects.Deopeia_Finance_Bff>("deopeia-finance-bff")
-    .WithEnvironment("Jwt__Key", jwtKey)
-    .WithEnvironment("Jwt__Issuer", jwtIssuer)
+    .WithJwt()
     .WithReference(identityApi)
     .WithReference(notificationHub)
     .WithReference(quoteApi)
--- src/Deopeia.AppHost/ResourceBuilderExtensions.cs
+++ src/Deopeia.AppHost/ResourceBuilderExtensions.cs
@@ -2,6 +2,65 @@ namespace Deopeia.AppHost;
 
 public static class ResourceBuilderExtensions
 {
+    private static IResourceBuilder<ParameterResource>? _jwtKey;
+    private static IResourceBuilder<ParameterResource>? _jwtIssuer;
+    private static IResourceBuilder<ParameterResource>? _minIOEndpoint;
+    private static IResourceBuilder<ParameterResource>? _minIOAccessKey;
+    private static IResourceBuilder<ParameterResource>? _minIOSecretKey;
+    private static IResourceBuilder<KafkaServerResource>? _kafka;
+
+    public static IResourceBuilder<TDestination> WithJwt<TDestination>(
+        this IResourceBuilder<TDestination> builder
+    )
+        where TDestination : ProjectResource
+    {
+        _jwtKey ??= builder.ApplicationBuilder.AddParameter("JwtKey");
+        _jwtIssuer ??= builder.ApplicationBuilder.AddParameter("JwtIssuer");
+
+        return builder
+            .WithEnvironment("Jwt__Key", _jwtKey)
+            .WithEnvironment("Jwt__Issuer", _jwtIssuer);
+    }
+
+    public static IResourceBuilder<TDestination> WithMinIO<TDestination>(
+        this IResourceBuilder<TDestination> builder
+    )
+        where TDestination : ProjectResource
+    {
+        _minIOEndpoint ??= builder.ApplicationBuilder.AddParameter("MinIOEndpoint");
+        _minIOAccessKey ??= builder.ApplicationBuilder.AddParameter("MinIOAccessKey");
+        _minIOSecretKey ??= builder.ApplicationBuilder.AddParameter("MinIOSecretKey");
+
+        return builder
+            .WithEnvironment("MinIO__Endpoint", _minIOEndpoint)
+            .WithEnvironment("MinIO__AccessKey", _minIOAccessKey)
+            .WithEnvironment("MinIO__SecretKey", _minIOSecretKey);
+    }
+
+    public static IResourceBuilder<TDestination> WithKafka<TDestination>(
+        this IResourceBuilder<TDestination> builder
+    )
+        where TDestination : ProjectResource
+    {
+        if (_kafka is null)
+        {
+            _kafka = builder
+                .ApplicationBuilder.AddKafka("kafka")
+                .WithKafkaUI(x => x.WithHostPort(9100))
+                .WithDataVolume();
+        }
+
+        return builder.WithReferenceAndWaitFor(_kafka);
+    }
+
+    public static IResourceBuilder<TDestination> WithProxyEndpoint<TDestination>(
+        this IResourceBuilder<TDestination> builder
+    )
+        where TDestination : ProjectResource
+    {
+        return builder.WithEnvironment("Proxy", builder.GetEndpoint("http"));
+    }
+
     public static IResourceBuilder<TDestination> WithReferenceAndWaitFor<TDestination>(
         this IResourceBuilder<TDestination> builder,
         IResourceBuilder<IResourceWithConnectionString> source,
