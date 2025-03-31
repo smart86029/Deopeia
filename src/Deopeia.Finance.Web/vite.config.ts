d07ddfa509c6a2e95e358836650ca574dc096bcc
Bill Hsu <smart86029@gmail.com>
2025-03-31 17:49:03 +0800

    chore(finance): auto import CDN

--- src/Deopeia.Finance.Web/vite.config.ts
+++ src/Deopeia.Finance.Web/vite.config.ts
@@ -5,6 +5,12 @@ import externalGlobals from 'rollup-plugin-external-globals';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import packageJson from './package.json' with { type: 'json' };
+
+const getVersion = (
+  name: 'vue' | 'vue-i18n' | 'vue-router' | 'axios' | 'element-plus' | 'pinia',
+) => packageJson.dependencies[name].replace(/^[^0-9]*/, '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +23,24 @@ export default defineConfig({
     Components({
       dts: './src/components.d.ts',
     }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          css: [
+            `//cdn.jsdelivr.net/npm/element-plus@${getVersion('element-plus')}/dist/index.min.css`,
+          ],
+          js: [
+            `//cdn.jsdelivr.net/npm/vue@${getVersion('vue')}/dist/vue.global.prod.js`,
+            '//cdn.jsdelivr.net/npm/vue-demi@0.14.10/lib/index.iife.min.js',
+            `//cdn.jsdelivr.net/npm/vue-i18n@${getVersion('vue-i18n')}/dist/vue-i18n.global.prod.js`,
+            `//cdn.jsdelivr.net/npm/vue-router@${getVersion('vue-router')}/dist/vue-router.global.min.js`,
+            `//cdn.jsdelivr.net/npm/axios@${getVersion('axios')}/dist/axios.min.js`,
+            `//cdn.jsdelivr.net/npm/element-plus@${getVersion('element-plus')}/dist/index.full.min.js`,
+            `//cdn.jsdelivr.net/npm/pinia@${getVersion('pinia')}/dist/pinia.iife.prod.js`,
+          ],
+        },
+      },
+    }),
     {
       ...externalGlobals({
         vue: 'Vue',
@@ -25,6 +49,7 @@ export default defineConfig({
         'vue-router': 'VueRouter',
         axios: 'axios',
         'element-plus': 'ElementPlus',
+        pinia: 'Pinia',
       }),
       enforce: 'post',
       apply: 'build',
