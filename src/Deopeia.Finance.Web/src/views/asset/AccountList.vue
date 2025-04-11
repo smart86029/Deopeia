7eefb11ab5af67f28a8a8c8adb67c2cc13514481
Bill Hsu <smart86029@gmail.com>
2025-04-11 18:19:16 +0800

    feat(finance): add currency icons

--- src/Deopeia.Finance.Web/src/views/asset/AccountList.vue
+++ src/Deopeia.Finance.Web/src/views/asset/AccountList.vue
@@ -1,14 +1,21 @@
 <template>
-  <div v-for="account in accounts" :key="account.currencyCode" class="account">
-    <span class="currency">
-      {{ currencies.find((x) => x.value === account.currencyCode)?.name }}
-    </span>
-    <span class="balance">{{ $n(account.balance, 'decimal') }}</span>
-    <span class="frozen">{{ $n(account.frozen, 'decimal') }}</span>
-    <el-button-group class="operation">
-      <el-button>{{ $t('trading.deposit') }}</el-button>
-      <el-button>{{ $t('trading.withdraw') }}</el-button>
-    </el-button-group>
+  <div class="account-list">
+    <div
+      v-for="account in accounts"
+      :key="account.currencyCode"
+      class="account"
+    >
+      <IconCurrency v-model="account.currencyCode" />
+      <span class="currency">
+        {{ currencies.find((x) => x.value === account.currencyCode)?.name }}
+      </span>
+      <span class="balance">{{ $n(account.balance, 'decimal') }}</span>
+      <span class="frozen">{{ $n(account.frozen, 'decimal') }}</span>
+      <el-button-group class="operation">
+        <el-button>{{ $t('trading.deposit') }}</el-button>
+        <el-button>{{ $t('trading.withdraw') }}</el-button>
+      </el-button-group>
+    </div>
   </div>
 </template>
 
@@ -23,9 +30,15 @@ assetApi.getList().then((x) => (accounts.value = x.data));
 </script>
 
 <style lang="scss" scoped>
+.account-list {
+  flex: 1;
+}
+
 .account {
   display: flex;
   width: 100%;
+  align-items: center;
+  gap: 8px;
 }
 
 .currency {
